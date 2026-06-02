c31e58964854e5f5f92b709553cec7f27840d8fd
Jefrin-S7 <jefrinjef255@gmail.com>
2026-06-02 22:37:56 +0530

    embedding debug

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -2,7 +2,6 @@ import os
 import fitz
 import numpy as np
 from pinecone import Pinecone, ServerlessSpec
-import requests
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 
@@ -12,44 +11,21 @@ load_dotenv(os.path.join(BASE_DIR, "../.env.local"))
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 INDEX_NAME= os.getenv("PINECONE_INDEX", "doc-rag-index")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "all-MiniLM-L6-v2")
-HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 1200))
 CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 150))
 TOP_K = int(os.getenv("TOP_K", 5))
 
 
-# _model = None
-
-HF_API_URL = f"https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/{EMBEDDING_MODEL}"
+_model = None
 
 #Embedding model
 
-# def get_model() -> SentenceTransformer:
-#     global _model
-#     if _model is None:
-#         print(f"[Embeddings] Loading model: {EMBEDDING_MODEL}", flush=True)
-#         _model = SentenceTransformer(EMBEDDING_MODEL)
-#     return _model
-
-#get embedding model from huggingface api
-def get_embedding(text: str) -> list:
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
-    response = requests.post(HF_API_URL, headers=headers, json={"inputs": text, "options": {"wait_for_model": True}})
-    response.raise_for_status()
-    embedding = response.json()
-
-    if isinstance(embedding[0], list):
-        embedding = embedding[0]
-    
-    return embedding
-
-#get embedding for multiple texts
-def get_embeddings_batch(texts: list) ->list:
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
-    response = requests.post(HF_API_URL, headers=headers, json={"inputs": texts, "options": {"wait_for_model": True}})
-    response.raise_for_status()
-    return response.json()
-
+def get_model() -> SentenceTransformer:
+    global _model
+    if _model is None:
+        print(f"[Embeddings] Loading model: {EMBEDDING_MODEL}", flush=True)
+        _model = SentenceTransformer(EMBEDDING_MODEL)
+    return _model
 
 #Pinecone 
 
@@ -92,61 +68,8 @@ def chunk_text(text: str) -> list:
 
 #Build Index
 
-# def build_index(pdf_files: list) -> tuple:
-#     model = get_model()
-#     all_chunks = []
-
-#     for pdf in pdf_files:
-#         print(f"[Chunking] {pdf['name']}", flush=True)
-#         text = extract_text(pdf["bytes"])
-#         chunks = chunk_text(text)
-#         print(f" -> {len(chunks)} chunks", flush=True)
-#         for chunk in chunks:
-#             all_chunks.append({
-#                 "text": chunk,
-#                 "source": pdf["name"],
-#             })
-
-
-#     print(f"\n[Embeddings] Total chunks: {len(all_chunks)}", flush=True)
-#     print("[Embeddings] Creating embeddings...", flush=True)
-
-#     texts = [c["text"] for c in all_chunks]
-#     embeddings = model.encode(
-#         texts,
-#         show_progress_bar=True,
-#         batch_size=64
-#     )
-
-#     print("[Pinecone] Uploading vectors...", flush=True)
-#     index = get_pinecone_index()
-#     batch_size = 200
-
-#     for i in range(0, len(all_chunks), batch_size):
-#         batch_chunks = all_chunks[i:i + batch_size]
-#         batch_embeds = embeddings[i:i + batch_size]
-#         vectors = [
-#             {
-#                 "id": str(i + j),
-#                 "values": emb.tolist(),
-#                 "metadata": {
-#                     "text": chunk["text"],
-#                     "source": chunk["source"],
-#                 }
-#             }
-#             for j, (chunk, emb) in enumerate(zip(batch_chunks, batch_embeds))
-#         ]
-#         index.upsert(vectors=vectors)
-#         print(f"[Pinecone] Batch {i // batch_size +1}", flush=True)
-
-#     print(f"[Pinecone] Done - {len(all_chunks)} vectors stored", flush=True)
-
-#     return index, all_chunks
-
-#Build index with hugging face
-
 def build_index(pdf_files: list) -> tuple:
-    import time
+    model = get_model()
     all_chunks = []
 
     for pdf in pdf_files:
@@ -159,59 +82,58 @@ def build_index(pdf_files: list) -> tuple:
                 "text": chunk,
                 "source": pdf["name"],
             })
-    print(f"\n[Embeddings] Total chunks: {len(all_chunks)}", flush=True) 
-    print("[Embeddings] Creating embeddings via Hugging Face API..", flush=True)
 
+
+    print(f"\n[Embeddings] Total chunks: {len(all_chunks)}", flush=True)
+    print("[Embeddings] Creating embeddings...", flush=True)
+
+    texts = [c["text"] for c in all_chunks]
+    embeddings = model.encode(
+        texts,
+        show_progress_bar=True,
+        batch_size=64
+    )
+
+    print("[Pinecone] Uploading vectors...", flush=True)
     index = get_pinecone_index()
-    batch_size = 50
+    batch_size = 200
+
+    # for attempt in range(3):
+    # try:
+    #     index.upsert(vectors=vectors)
+    #     break
+    # except Exception as e:
+    #     print(f"Retry {attempt + 1}: {e}", flush=True)
+    #     time.sleep(2)
 
     for i in range(0, len(all_chunks), batch_size):
         batch_chunks = all_chunks[i:i + batch_size]
-        texts = [c["text"] for c in batch_chunks]
-
-        for attempt in range(3):
-            try:
-                embeddings = get_embeddings_batch(texts)
-                break
-            except Exception as e:
-                print(f"[HF] Retry {attempt + 1}: {e}", flush=True)
-                time.sleep(3)
-        
+        batch_embeds = embeddings[i:i + batch_size]
         vectors = [
             {
                 "id": str(i + j),
-                "values": emb if isinstance(emb[0], float) else emb[0],
+                "values": emb.tolist(),
                 "metadata": {
                     "text": chunk["text"],
                     "source": chunk["source"],
                 }
             }
-            for j, (chunk, emb) in enumerate(zip(batch_chunks, embeddings))
+            for j, (chunk, emb) in enumerate(zip(batch_chunks, batch_embeds))
         ]
-
-        #upload to pinecone
-        for attempt in range(3):
-            try:
-                index.upsert(vectors=vectors)
-                print(f"[Pinecone] Batch {i // batch_size + 1}", flush=True)
-                break
-            except Exception as e:
-                print(f"[Pinecone] Retry {attempt + 1}: {e}", flush=True)
-                time.sleep(2)
+        index.upsert(vectors=vectors)
+        print(f"[Pinecone] Batch {i // batch_size +1}", flush=True)
 
     print(f"[Pinecone] Done - {len(all_chunks)} vectors stored", flush=True)
-    return index, all_chunks
 
+    return index, all_chunks
+    
 
 # Search similar chunks
 
 def search(query: str, top_k: int = TOP_K) -> list:
-    # model = get_model()
-    # index = get_pinecone_index()
-    # query_vec = model.encode([query])[0].tolist()
-
+    model = get_model()
     index = get_pinecone_index()
-    query_vec = get_embedding(query)
+    query_vec = model.encode([query])[0].tolist()
 
     results = index.query(vector=query_vec, top_k=top_k, include_metadata=True)
 
@@ -219,7 +141,6 @@ def search(query: str, top_k: int = TOP_K) -> list:
         {
             "text": match["metadata"]["text"],
             "source": match["metadata"]["source"],
-            "score": float(match["score"])
         }
         for match in results["matches"]
     ]
